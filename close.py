cdad63f3b417846b0bd922844221046e11a84578
Bhanu <bhanuprakashaleti06@gmail.com>
2026-06-29 23:40:58 +0530

    Removed test files from root

--- close.py
+++
@@ -1,7 +0,0 @@
-import sys
-from support_agent.memory import close_ticket
-
-ticket_id = int(sys.argv[1])
-resolution = sys.argv[2]
-close_ticket(ticket_id, resolution)
-print(f"Closed ticket {ticket_id} and updated the customer's summary.")
--- inspect_summaries.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-from support_agent import DB_PATH
-conn = sqlite3.connect(DB_PATH)
-for cid, name, summary in conn.execute(
-    "SELECT customer_id, name, summary FROM customers"
-):
-    print(cid, name, "->", repr(summary))
